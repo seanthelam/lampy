56c73d2ca2f53aaaaa06410412d42eda559e52bd
seanthelam <144201405+seanthelam@users.noreply.github.com>
2024-06-26 10:38:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup, find_packages
 
+from lampy import __minimum_python_version__
+
+setup(
+    name='lampy',
+    version=__minimum_python_version__,
+    description='A practice Python package.',
+
+    url='https://github.com/seanthelam/lampy',
+    author='Sean Lam',
+    author_email='s_lam2023@coloradocollege.edu',
+
+    packages=find_packages(exclude=['tests', 'tests.*']),
+)
