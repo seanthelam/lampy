438407d2863092b367d63f7b6d8027b6384ff22a
seanthelam <144201405+seanthelam@users.noreply.github.com>
2024-07-08 11:18:35 -0600

    Add files via upload

--- lampy/__init__.py
+++ lampy/__init__.py
@@ -1,7 +1,12 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-import sys
+from .example_mod import do_primes
+from importlib.metadata import version as _version, PackageNotFoundError
+try:
+    __version__ = _version(__name__)
+except PackageNotFoundError:
+    pass
 
-__minimum_python_version__ = "3.10"
 
+# Then you can be explicit to control what ends up in the namespace,
+__all__ = ['do_primes']
 
 
