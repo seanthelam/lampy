6978f85fb075b7f64829bb025de31219ec82628c
seanthelam <144201405+seanthelam@users.noreply.github.com>
2024-07-08 11:07:21 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import setup
 
-from lampy import __minimum_python_version__
-
-setup(
-    name='lampy',
-    version=__minimum_python_version__,
-    description='A practice Python package.',
-
-    url='https://github.com/seanthelam/lampy',
-    author='Sean Lam',
-    author_email='s_lam2023@coloradocollege.edu',
-
-    packages=find_packages(exclude=['tests', 'tests.*']),
-)
+setup()
